8fc4dadbf5b3b92e37999c453503e48977c86d48
Umer <umerqaisar786@gmail.com>
2023-04-12 13:36:57 +0500

    Added validation for cards

--- app/javascript/custom/card.js
+++ app/javascript/custom/card.js
@@ -1,1 +1,19 @@
-     alert('Add Card')
+// Get the input field element by its ID
+const cardNumberInput = document.getElementById('cardNumber');
+
+// Add an event listener for input change
+cardNumberInput.addEventListener('input', (event) => {
+    // Remove all non-digit characters from the input value
+    const cardNumber = event.target.value.replace(/\D/g, '');
+
+    // Add a space after every 4 digits
+    const formattedCardNumber = cardNumber.replace(/(\d{4})/g, '$1 ');
+
+    // Update the input value with the formatted card number
+    event.target.value = formattedCardNumber.trim();
+
+    // Limit the input to a maximum of 16 digits
+    if (cardNumber.length > 16) {
+        event.target.value = formattedCardNumber.slice(0, 19).trim();
+    }
+});
