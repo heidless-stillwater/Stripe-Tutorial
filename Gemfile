1811102f1c0ce77835dc34380ffd7c0e64a46485
Umer <umerqaisar786@gmail.com>
2023-04-12 14:07:44 +0500

    Configured .env

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ ruby "3.1.2"
 
 gem 'stripe'
 
+gem 'dotenv-rails'
+
+gem 'byebug'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.3"
 
--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,7 +2,7 @@ class ChargesController < ApplicationController
 
   def create
     flash[:notice] = "Creating..........................................."
-    flash[:alert] = params
+    flash[:alert] = ENV['STRIPE_PUBLISH_KEY']
     redirect_back fallback_location: root_path
   end
 
